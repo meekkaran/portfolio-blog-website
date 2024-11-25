7afd85ecdaf40768c2570b4faaee0b96514cf425
Meek Jerop <meekjerop@microsoft.com>
2024-11-25 15:29:25 +0300

    adding google insights on my website

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
+          {/* Existing Favicon and Metadata */}
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/meekkaran.png" />
           <link
             rel="icon"
@@ -23,14 +24,30 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          
+          {/* Google Analytics Script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-VTWSRXJNZN');
+              `,
+            }}
+          ></script>
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
